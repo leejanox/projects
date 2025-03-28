6ff181db979b5db5c86d5bff59fd6fd6811893d7
goeun <rhdms6748@gmail.com>
2025-03-28 22:27:21 +0900

    250328

--- apps/haneum/src/components/UI/searchBox.tsx
+++ apps/haneum/src/components/UI/searchBox.tsx
@@ -1,9 +1,53 @@
 import React from "react"
 import styled from "styled-components"
+import { FaSearch } from "react-icons/fa"
 
 function SearchBox({placeholder,...props}:React.InputHTMLAttributes<HTMLInputElement>) {
-    return <input type="text" placeholder={placeholder} {...props}/>
+    return (
+        <URL_SB>
+            <input type="text" placeholder={placeholder} {...props}/>
+            <button type="submit" aria-label="search" onClick={(e)=>{e.preventDefault(); alert("search")}}>
+                <FaSearch size={30} color="#15051f" />
+            </button>
+        </URL_SB>
+    )
 }
 
 export const URL_SB = styled(SearchBox)`
+    position: relative;
+    width: 50rem;
+    height: 3rem;
+    border-radius: 1rem;
+    border: none;
+    box-shadow:
+        0 0 0.2rem rgba(0, 0, 0, 0.3),
+        0 0 0.3rem rgba(0, 0, 0, 0.4),
+        0 0 0.1rem rgba(0, 0, 0, 0.5) inset;
+    padding: 0.3rem 1rem;
+    padding-right: 3rem;
+
+    input {
+        width: 100%;
+        height: 100%;
+        font-size: 1rem;
+        font-weight: 400;
+        color: #777;
+        outline: none;
+        border: none;
+        background-color: transparent;
+        padding: 0;
+    }
+    
+    input::placeholder {
+        color: rgba(0, 0, 0, 0.3);
+        font-size: 0.8rem;
+        font-weight: 600;
+    }
+
+    input:focus {
+        box-shadow:
+            0 0 0.2rem rgba(0, 0, 0, 0.6),
+            0 0 0.3rem rgba(0, 0, 0, 0.7),
+            0 0 0.1rem rgba(0, 0, 0, 0.8) inset;
+    }
 `
--- apps/haneum/src/pages/homePage.tsx
+++ apps/haneum/src/pages/homePage.tsx
@@ -1,10 +1,20 @@
 import MainLayout from "@components/layouts/mainLayout"
+//import { URL_SB } from "@components/UI/searchBox"
 
 export default function HomePage() {
   return (
     <MainLayout>
         <div className="home-content">
-            home
+          <section>
+            <div className="section-1">
+            </div>
+            <div className="section-2">
+
+            </div>
+            <div className="section-3">
+
+            </div>
+          </section>
         </div>
     </MainLayout>
   )
