636eab8e9a8a0b4e1e6587ee46bd115c850d74f4
goeun <rhdms6748@gmail.com>
2025-04-01 20:49:54 +0900

    250401

--- apps/te/src/main.tsx
+++ apps/te/src/main.tsx
@@ -1,10 +1,13 @@
+import { Loading } from "@components/loading";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import "./global.scss";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Router>
+            <Loading/>
         </Router>
     </StrictMode>
 )
